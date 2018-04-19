c5a6f1960a2316645985e4d0dae2488206f4272e
Stuart Harrison <stu@miserablepig.com>
2018-04-19 13:01:32 +0100

    Mike

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
     <!-- MAIN CONTENT -->
 	<div class="wrapper on-light">
 	<div class="separator margin30"></div>
-		<h2 class="text-align-center margin20">Welcome to Register Tracking</h2>
+		<h2 class="text-align-center margin20">Welcome Mike, to Register Tracking</h2>
 		<h3 class="text-align-center italic gray">Questions? <a class="link dark" href="mailto:help@registertracking.com">Yes please!</a></h3>
 		<div class="clear separator margin20 nobottommargin"></div>
 	</div>
